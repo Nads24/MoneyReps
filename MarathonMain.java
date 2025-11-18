8f252c8ac99f74192aae6373fc6207127291c50b
Aminul Goldar <80048839+Nads24@users.noreply.github.com>
2025-11-18 11:05:03 -0500

    Update MarathonMain.java

--- MarathonMain.java
+++ MarathonMain.java
@@ -1,17 +1,16 @@
-package main;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Git Repository: https://github.com/Vanier-ComputerScience/420-sf3-re-assignmnet-02-2025-fall-jameshuyha.git
- * @author Huy James Vien Ha (6324151)
+ * GitHub:
+ * https://github.com/Vanier-ComputerScience/420-sf3-re-assignmnet-02-2025-fall-Nads24
+ *
+ * Author: Nads
  */
 public class MarathonMain extends Application {
 
-    // Loads program
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/marathon.fxml"));
